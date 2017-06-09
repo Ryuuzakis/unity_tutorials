e8541f3ceec2e467bbbd805f26f294d7798c6b16
Jonathan <jonathan@energic.io>
2017-06-09 18:34:54 +0200

    roll a ball

---
+++ roll_a_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedX;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> (); 
+	}
+
+	void FixedUpdate() {
+		// called before any physics calculation
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speedX);
+	}
+
+}
